13a8b58819183b3cf3b4a1bb1e90076ac20fc58e
valerysamovich <valerysamovich@gmail.com>
2014-06-27 02:20:58 -0400

    Add @Override annotation example

---
+++ src/extra1/override/OverrideSubClass.java
@@ -0,0 +1,9 @@
+package extra1.override;
+
+public class OverrideSubClass extends OverrideSuperClass {
+
+    @Override
+    public void hello() {
+        System.out.println("Hello from OverrideSubClass!");
+    }
+}
---
+++ src/extra1/override/OverrideSuperClass.java
@@ -0,0 +1,8 @@
+package extra1.override;
+
+public class OverrideSuperClass {
+    
+    public void hello() {
+        System.out.println("Hello from OverrideSuperClass!");
+    }
+}

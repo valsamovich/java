76d6fcce9bc9ea8e9fa7ed1036ee4f454e918f2a
samov004 <valerysamovich@gmail.com>
2014-06-28 00:43:07 -0400

    Add new static example

---
+++ src/day3/staticex2/MyStaticTester.java
@@ -0,0 +1,12 @@
+package day3.staticex2;
+
+public class MyStaticTester {
+
+    public static void main(String[] args) {
+        
+        System.out.println(MyStaticExample.PLACEHOLDER);
+        MyStaticExample.test();
+
+    }
+
+}

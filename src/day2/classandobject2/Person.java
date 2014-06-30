d9a9c9a5db8286581caa2cb3ea0560112375bd13
valerysamovich <valerysamovich@gmail.com>
2014-06-30 03:50:26 -0400

    Update class and object example2

--- src/day2/classandobject2/Person.java
+++ src/day2/classandobject2/Person.java
@@ -2,4 +2,24 @@ package day2.classandobject2;
 
 public class Person {
 
+    // Instance variables
+    String firstName = "Jim";
+    String lastName = "Norris";
+    int age = 12;
+
+    // Constructor with 3 arguments
+    public Person(String a, String b, int myAge) {
+        this.firstName = a;
+        this.lastName = b;
+        this.age = myAge;
+    }
+    
+    void writeName() {
+        // Writes the first name
+        System.out.println(firstName);
+
+        // Alternatively, combine with strings with +
+        System.out.println(firstName + " " + lastName + " is " + age);
+    }
+
 }

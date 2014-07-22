2ab8d8d6f613c809a5ae549e87d9334845f8bf82
valerysamovich <valerysamovich@gmail.com>
2014-07-21 22:55:10 -0400

    Add if and if-else examples

--- src/com/valerysamovich/java/trail/basic/blocks/BlockExample.java
+++ src/com/valerysamovich/java/trail/basic/blocks/BlockExample.java
@@ -13,15 +13,14 @@ public class BlockExample {
         
         boolean condition = true;
         
-        // begin block 1
+        // start block 1
         if (condition) {
             System.out.println("Condition is true.");
-        // end block 1 and begin block 2
+        // end block 1 and start block 2
         } else {
             System.out.println("Condition is false.");
         }
         // end block 2
-
     }
 
 }
---
+++ src/com/valerysamovich/java/trail/basic/statements/ifstatement/IfExample.java
@@ -0,0 +1,18 @@
+package com.valerysamovich.java.trail.basic.statements.ifstatement;
+
+public class IfExample {
+
+    public static void main(String[] args) {
+        
+        int testScore = 81;
+        String grade = "";
+        
+        if (testScore >= 50) {
+            grade = "pass";
+        }
+        
+        System.out.println("Grade: " + grade);
+
+    }
+
+}

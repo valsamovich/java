99c3e10b1cf5deb3975beb29adf21a754e041834
valerysamovich <valerysamovich@gmail.com>
2014-08-21 00:24:37 -0400

    Add JavaDoc

--- src/com/valerysamovich/java/advance/collections/interfaces/map/CollectionsDemo.java
+++ src/com/valerysamovich/java/advance/collections/interfaces/map/CollectionsDemo.java
@@ -22,15 +22,21 @@ public class CollectionsDemo {
      */
     public static void main(String[] args) {
         
+        // Create the map
         Map m1 = new HashMap();
+        // Add values to the map
         m1.put("Zara", "8");
         m1.put("Mahnaz", "31");
         m1.put("Ayan", "12");
         m1.put("Daisy", "14");
         
+        // Print the results
         System.out.println();
         System.out.println("Map Elements");
         System.out.println("\t" + m1);
+        
+        // Return the size of the map
+        System.out.println(m1.size());
 
     }
 

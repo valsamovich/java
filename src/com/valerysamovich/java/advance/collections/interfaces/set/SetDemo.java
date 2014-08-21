d0f08622bb2c29f7669f9527e02961adf45260c3
valerysamovich <valerysamovich@gmail.com>
2014-08-21 00:47:44 -0400

    Finish the Set Example

--- src/com/valerysamovich/java/advance/collections/interfaces/set/SetDemo.java
+++ src/com/valerysamovich/java/advance/collections/interfaces/set/SetDemo.java
@@ -9,6 +9,7 @@ package com.valerysamovich.java.advance.collections.interfaces.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetDemo {
     
@@ -22,5 +23,23 @@ public class SetDemo {
         int count[] = {34, 22, 10, 60, 30, 22};
         // Create the Set Integer Collection
         Set<Integer> set = new HashSet<Integer>();
+        // Try catch block
+        try {
+            // For loop
+            for (int i = 0; i < 5; i++) {
+                set.add(count[i]);
+            }
+            // Prints the values from int array
+            System.out.println(set);
+            
+            // TreeSet
+            TreeSet sortedSet = new TreeSet<Integer>(set);
+            System.out.println("The sorted list is: " + sortedSet);
+            System.out.println("The first element of the set is: " +
+                    (Integer)sortedSet.first());
+            
+        } catch (Exception e) {
+            // statement(s)
+        }
     }
 }

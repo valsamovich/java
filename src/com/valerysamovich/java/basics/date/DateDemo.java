c5e88bc934fac3b184488fbf726a5ad5602f9d19
valerysamovich <valerysamovich@gmail.com>
2014-09-03 15:50:35 -0400

    Add Date Example

---
+++ src/com/valerysamovich/java/basics/date/DateDemo.java
@@ -0,0 +1,27 @@
+/**
+ * @file DateDemo.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 009/03/2014
+ */
+
+package com.valerysamovich.java.basics.date;
+
+import java.util.Date;
+
+public class DateDemo {
+    
+    /**
+     * Main Method
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Instantiate the Date Object
+        Date date = new Date();
+        
+        // Display Date and Time using toString();
+        System.out.println(date.toString());
+
+    }
+
+}

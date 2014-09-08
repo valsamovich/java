a9663ef1f3eac0a164fee063c0a07a1d2c829ac7
samov004 <valerysamovich@gmail.com>
2014-09-07 23:18:09 -0400

    Add Employee Class

---
+++ src/com/valerysamovich/java/advance/serialization/serialize/SerializeDemo.java
@@ -0,0 +1,18 @@
+/**
+ * @file SerializeDemo.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 08/27/2014
+ */
+
+package com.valerysamovich.java.advance.serialization.serialize;
+
+public class SerializeDemo {
+
+    public static void main(String[] args) {
+        
+        Employee e  = new Employee
+
+    }
+
+}

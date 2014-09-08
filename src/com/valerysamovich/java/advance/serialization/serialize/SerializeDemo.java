c8ca4e2b1e5520104ff24ea1b65aeb97f1a62e44
samov004 <valerysamovich@gmail.com>
2014-09-07 23:23:16 -0400

    Add Serialize example

--- src/com/valerysamovich/java/advance/serialization/serialize/SerializeDemo.java
+++ src/com/valerysamovich/java/advance/serialization/serialize/SerializeDemo.java
@@ -7,11 +7,32 @@
 
 package com.valerysamovich.java.advance.serialization.serialize;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializeDemo {
 
     public static void main(String[] args) {
         
-        Employee e  = new Employee
+        // Create the instance of the Employee object
+        Employee e  = new Employee();
+        e.name ="John Doe";
+        e.address = "Florida, USA";
+        e.SSN = 1112223333;
+        e.number = 101;
+        
+        try {
+           FileOutputStream fileOut =
+           new FileOutputStream("/tmp/employee.ser");
+           ObjectOutputStream out = new ObjectOutputStream(fileOut);
+           out.writeObject(e);
+           out.close();
+           fileOut.close();
+           System.out.printf("Serialized data is saved in /tmp/employee.ser");
+        } catch(IOException i) {
+            i.printStackTrace();
+        }
 
     }
 

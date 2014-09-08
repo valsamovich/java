da0299fbce7077b3ed01c6c3c4068b7c757ab589
valerysamovich <valerysamovich@gmail.com>
2014-09-08 16:45:20 -0400

    Finish the JUnit test for StringHelper

--- test/com/valerysamovich/java/basics/exercises/StringHelperTest.java
+++ test/com/valerysamovich/java/basics/exercises/StringHelperTest.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class StringHelperTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testTruncateAInFirstTwoPositions() {
+        
+        // Create the new StringHelper object
+        StringHelper stringHelper = new StringHelper();
+        
+        // Create the var and Call the method for test with String Value
+        String actualValue = stringHelper.truncateAInFirstTwoPositions("BACD");
+        // Create the var and assign the value
+        String expectedValue = "BCD";
+        
+        // Assert the values by assertEquals() method
+        assertEquals(actualValue, expectedValue);
     }
 
 }

3f071a7957e2259d65a0f90b78c522e8bade0917
samov004 <valerysamovich@gmail.com>
2014-09-08 15:49:29 -0400

    Add Trancate Example

---
+++ src/com/valerysamovich/java/basics/exercises/StringHelper.java
@@ -0,0 +1,23 @@
+package com.valerysamovich.java.basics.exercises;
+
+public class StringHelper {
+    
+    public String truncateAInFirstTwoPositions(String str) {
+        
+        // If string equal 0
+        if (str.length() == 0) {
+            return str;
+        }
+        
+        // If string more then 2 char
+        if (str.length() < 2) {
+            return str.replace("A", "");
+        }
+        
+        String first2Char = str.substring(0, 2);
+        String afterFirst2Char = str.substring(2);
+        
+        // Return the result
+        return first2Char.replaceAll("A", "") + afterFirst2Char;
+    }
+}

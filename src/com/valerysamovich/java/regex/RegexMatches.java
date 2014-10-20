f8b07521a03b9f91eede3691a7cd1a60d5d09b74
valerysamovich <valerysamovich@gmail.com>
2014-10-19 22:54:05 -0400

    Add Regex Package and start

---
+++ src/com/valerysamovich/java/regex/RegexMatches.java
@@ -0,0 +1,12 @@
+package com.valerysamovich.java.regex;
+
+public class RegexMatches {
+    
+    public static void main(String args[]) {
+        
+        // String to be scanned to find the pattern
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(.*)(\\d+)(.*)";
+    }
+
+}

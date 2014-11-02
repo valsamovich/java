f5f3fc35e22cf096b91cfaf2e0b05c7cd60fa76f
valerysamovich <valerysamovich@gmail.com>
2014-11-01 23:16:16 -0400

    Add socket example

---
+++ src/com/valerysamovich/java/advanced/networking/GreetingClient.java
@@ -0,0 +1,21 @@
+/**
+ * @file GreetingClient.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/1/2014
+ * 
+ * GreetingClient is a client program that connects to a server by using a 
+ * socket and send a greeting, and then waits for response.
+ */
+
+package com.valerysamovich.java.advanced.networking;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+    
+    
+
+    }
+
+}

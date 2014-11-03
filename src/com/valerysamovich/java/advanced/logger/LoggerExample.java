25b470b25cbaf81a541dfed906d2fc7233819816
valerysamovich <valerysamovich@gmail.com>
2014-11-03 13:35:31 -0500

    Finish the LoggerExample and add some Javadoc

--- src/com/valerysamovich/java/advanced/logger/LoggerExample.java
+++ src/com/valerysamovich/java/advanced/logger/LoggerExample.java
@@ -15,15 +15,27 @@ import java.util.logging.*;
 
 public class LoggerExample {
     
-    // Create the LOGGER Object
+    // Create the LOGGER Object using getLogger static method
     private static final Logger LOGGER = 
             Logger.getLogger(LoggerExample.class.getName());
     
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws SecurityException, IOException {
         
+        // LOGGER message to find logger by the name as a parameter
         LOGGER.info("Logger Name: " + LOGGER.getName());
+        // This instance method is used to log a WARNING message
         LOGGER.warning("Can cause  ArrayIndexOutOfBoundsException");
 
+        // An array of the size 3
+        int[] a = {1, 2, 3};
+        int index = 4;
+        LOGGER.config("Index is set to" + index);
+        
+        try {
+            System.out.println(a[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            LOGGER.log(Level.SEVERE, "Exception occur", e);
+        }
     }
 
 }

93977378457016d5dd1b0c77b2cbbf3cf0e83dcc
valerysamovich <valerysamovich@gmail.com>
2014-11-05 00:18:16 -0500

    Add HandlerExample.java

---
+++ src/com/valerysamovich/java/advanced/logger/HandlerExample.java
@@ -0,0 +1,24 @@
+/**
+ * @file HandlerExample.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/5/2014
+ * 
+ * Example of logger class with LOGGER object which is used to log messages.
+ */
+
+package com.valerysamovich.java.advanced.logger;
+
+import java.util.logging.*;
+
+public class HandlerExample {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggerExample.class.getName());
+    
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}

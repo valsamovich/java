2e54acedb35d03d10f793e853005a3a26e6e24ef
valerysamovich <valerysamovich@gmail.com>
2014-11-05 00:28:56 -0500

    Modify HandlerExample.java

--- src/com/valerysamovich/java/advanced/logger/HandlerExample.java
+++ src/com/valerysamovich/java/advanced/logger/HandlerExample.java
@@ -9,6 +9,7 @@
 
 package com.valerysamovich.java.advanced.logger;
 
+import java.io.IOException;
 import java.util.logging.*;
 
 public class HandlerExample {
@@ -17,7 +18,30 @@ public class HandlerExample {
     
     public static void main(String[] args) {
         
+        /*
+         * A ConsoleHandler records all the log messages to System.err. By 
+         * default, a Logger is associated with this handler.
+         */
+        Handler consoleHandler = null;
         
+        /*
+         * A FileHandler is used to record all the log messages to a specific 
+         * file or to a rotating set of files.
+         */
+        Handler fileHandler = null;
+        
+        try {
+            //Creating consoleHandler and fileHandler
+            consoleHandler = new ConsoleHandler();
+            fileHandler  = new FileHandler("./javacodegeeks.log");
+            
+            //Assigning handlers to LOGGER object
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.addHandler(fileHandler);
+
+        } catch (IOException exception) {
+            
+        }
 
     }
 

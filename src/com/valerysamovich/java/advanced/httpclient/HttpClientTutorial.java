596f99641f143d5662123ca121975c56bdb8f0bf
samov004 <valerysamovich@gmail.com>
2014-11-19 02:51:53 -0500

    Start the HttpClient Example

---
+++ src/com/valerysamovich/java/advanced/httpclient/HttpClientTutorial.java
@@ -0,0 +1,18 @@
+/**
+ * @file HttpClientTutorial.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/19/2014
+ */
+
+package com.valerysamovich.java.advanced.httpclient;
+
+public class HttpClientTutorial {
+
+    private static String url = "http://www.apache.org/";
+
+    public static void main(String[] args) {
+
+        // Create
+    }
+}

2d41842163e93f40fae366762b0480eecf8368a6
samov004 <valerysamovich@gmail.com>
2014-11-23 15:05:49 -0500

    Started chaining Constructor Example

---
+++ src/com/valerysamovich/java/oop/constructors/chaining/GrandParent.java
@@ -0,0 +1,21 @@
+/**
+ * @file GrantParent.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/22/2014
+ *
+ * Java Constructor chaining.
+ */
+
+package com.valerysamovich.java.oop.constructors.chaining;
+
+public class GrandParent {
+
+    int classA;
+
+    // Constructor Method.
+    GrandParent(int constructorA) {
+        this.classA = constructorA;
+    }
+
+}
---
+++ src/com/valerysamovich/java/oop/constructors/chaining/Parent.java
@@ -0,0 +1,13 @@
+/**
+ * @file Parent.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 11/22/2014
+ *
+ * Java Constructor chaining.
+ */
+
+package com.valerysamovich.java.oop.constructors.chaining;
+
+public class Parent {
+}

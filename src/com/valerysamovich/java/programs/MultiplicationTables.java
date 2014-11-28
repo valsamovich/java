d81060998ef3e49cdda89f99d1488a93d0d932d5
samov004 <valerysamovich@gmail.com>
2014-11-28 02:56:54 -0500

    Finish MultiplicationTables Example

--- src/com/valerysamovich/java/programs/MultiplicationTables.java
+++ src/com/valerysamovich/java/programs/MultiplicationTables.java
@@ -28,8 +28,12 @@ public class MultiplicationTables {
         a = in.nextInt();
         b = in.nextInt();
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        for (c = a; c <= b; c++) {
+            System.out.println("Multiplication table of " + c);
+        }
 
+        for (d = 1; d <= 10; d++) {
+            System.out.println(c + " * " + d + " = " + (c * d));
+        }
     }
 }

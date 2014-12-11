ed7a98cb731946c9d882742560ecb609b69ddd73
samov004 <valerysamovich@gmail.com>
2014-12-11 16:58:52 -0500

    Add AssertThat example

--- test/com/valerysamovich/java/advanced/junit/MyClassTest.java
+++ test/com/valerysamovich/java/advanced/junit/MyClassTest.java
@@ -1,8 +1,10 @@
+
+
 package com.valerysamovich.java.advanced.junit;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MyClassTest {
     

3f3163bc41f8bbd22ffdad44ad14f684ff1af01f
samov004 <valerysamovich@gmail.com>
2014-12-16 12:16:34 -0500

    Add assert example with if

---
+++ test/com/valerysamovich/java/advanced/junit/AssertTest.java
@@ -0,0 +1,32 @@
+/**
+ * @file AssertTest.java
+ * @author Valery Samovich
+ * @version 1.0.0
+ * @date 12/16/2014
+ *
+ * Demonstrates the assert with if statement with input.
+ */
+
+package com.valerysamovich.java.advanced.junit;
+
+import java.util.Scanner;
+
+public class AssertTest {
+
+    public static void main(String[] args) {
+
+        // Create instance of Scanner
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number between 0 to 10: ");
+        int number = input.nextInt();
+
+        // Assert that the absolute value is >= 0
+        if (!(number >= 0 && number <= 10))
+            throw new AssertionError("Bad number: " + number);
+
+        // Print the response
+        System.out.printf("You entered %d\n", number);
+
+    }
+}

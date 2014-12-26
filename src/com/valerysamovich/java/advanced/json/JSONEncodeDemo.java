7bb36e36cda8d8ee1c3bd0b40b7ca0bf0c6f3ca5
Samovich <valerysamovich@gmail.com>
2014-12-25 23:48:20 -0500

    Add Encoding JSON in Java Example

--- src/com/valerysamovich/java/advanced/json/JSONEncodeDemo.java
+++ src/com/valerysamovich/java/advanced/json/JSONEncodeDemo.java
@@ -15,7 +15,6 @@ import org.json.simple.JSONObject;
 public class JSONEncodeDemo {
 
     public static void main(String[] args) {
-
         JSONObject obj = new JSONObject();
         
         obj.put("name", "foo");

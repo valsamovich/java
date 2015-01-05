fcc1ab058fe56c9ecaa44c69b2bba3bfd02b8a2e
Valery <valerysamovich@gmail.com>
2015-01-05 11:50:48 -0500

    Add counter Static example plus non Static

--- src/com/valerysamovich/java/oop/staticex/counter/Counter.java
+++ src/com/valerysamovich/java/oop/staticex/counter/Counter.java
@@ -9,7 +9,23 @@ package com.valerysamovich.java.oop.staticex.counter;
 
 public class Counter {
 
-    NonStaticCounter c1 = new NonStaticCounter();
-    NonStaticCounter c2 = new NonStaticCounter();
-    NonStaticCounter c3 = new NonStaticCounter();
+    public static void main(String[] args) {
+
+        System.out.println("Non Static Objects:");
+
+        // Non Static Object Counters
+        NonStaticCounter c1 = new NonStaticCounter();
+        NonStaticCounter c2 = new NonStaticCounter();
+        NonStaticCounter c3 = new NonStaticCounter();
+
+        System.out.println(" ");
+
+        System.out.println("Static Objects:");
+        // Static Object Counters
+        StaticCounter c4 = new StaticCounter();
+        StaticCounter c5 = new StaticCounter();
+        StaticCounter c6 = new StaticCounter();
+
+    }
+
 }

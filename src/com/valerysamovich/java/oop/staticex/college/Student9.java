df51741e24e336c8a01355bd820008752c04c21b
Valery <valerysamovich@gmail.com>
2015-01-06 17:00:41 -0500

    Start Example of Static Method

---
+++ src/com/valerysamovich/java/oop/staticex/college/Student9.java
@@ -0,0 +1,12 @@
+/**
+ * @file Counter.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2015/01/05
+ */
+
+package com.valerysamovich.java.oop.staticex.college;
+
+public class Student9 {
+
+}

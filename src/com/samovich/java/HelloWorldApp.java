d1c7387aeea567ab270e84e323e01f60517521fa
Valery <valerysamovich@gmail.com>
2015-01-09 10:50:15 -0500

    Start concurrency example and add the template

---
+++ src/com/samovich/java/HelloWorldApp.java
@@ -0,0 +1,20 @@
+/**
+ * @file Template.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2014/01/09
+ */
+
+package com.samovich.java;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+
+}
---
+++ src/com/samovich/java/basics/essential/concurrency/HelloRunnable.java
@@ -0,0 +1,12 @@
+/**
+ * @file HelloRunnable.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2014/01/09
+ */
+
+package com.samovich.java.basics.essential.concurrency;
+
+public class HelloRunnable {
+
+}

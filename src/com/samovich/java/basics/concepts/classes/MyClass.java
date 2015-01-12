d4f960d55b60706bc7d18170cb1e9bc9cbc37070
Samovich <valerysamovich@gmail.com>
2015-01-11 22:46:47 -0500

    Add Sleep thread and Class Declaration examples.

---
+++ src/com/samovich/java/basics/concepts/classes/MyClass.java
@@ -0,0 +1,17 @@
+/**
+ * @file MyClass.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2014/01/09
+ * 
+ *  Example of Class Declaration
+ */
+
+package com.samovich.java.basics.concepts.classes;
+
+/*
+ * Class declaration
+ */
+public class MyClass {
+    // Field(s), Constructor, and Method declarations
+}

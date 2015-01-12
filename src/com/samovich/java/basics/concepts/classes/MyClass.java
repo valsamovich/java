92317d3b7d6f1f7640e0a71c039c5f79f75b8517
Samovich <valerysamovich@gmail.com>
2015-01-11 22:50:27 -0500

    Add Javadoc for Class Declaration.

--- src/com/samovich/java/basics/concepts/classes/MyClass.java
+++ src/com/samovich/java/basics/concepts/classes/MyClass.java
@@ -5,6 +5,15 @@
  * @date 2014/01/09
  * 
  *  Example of Class Declaration
+ * 1. Modifiers such as public, private, and a number of others that you will 
+ *    encounter later.
+ * 2. The class name, with the initial letter capitalized by convention.
+ * 3. The name of the class's parent (superclass), if any, preceded by the 
+ *    keyword extends. A class can only extend (subclass) one parent.
+ * 4. A comma-separated list of interfaces implemented by the class, if any, 
+ *    preceded by the keyword implements. A class can implement more than one 
+ *    interface.
+The class body, surrounded by braces, {}.  
  */
 
 package com.samovich.java.basics.concepts.classes;

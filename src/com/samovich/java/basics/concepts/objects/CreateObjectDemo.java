98076b449777aa2c716795a68c2d5f57a606e0e0
Valery <valerysamovich@gmail.com>
2015-01-13 12:09:09 -0500

    Start Extra example for Object

---
+++ src/com/samovich/java/basics/concepts/objects/CreateObjectDemo.java
@@ -0,0 +1,16 @@
+/**
+ * @file CreateObjectDemo.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2014/01/13
+ */
+
+package com.samovich.java.basics.concepts.objects;
+
+public class CreateObjectDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/com/samovich/java/basics/concepts/objects/Point.java
@@ -0,0 +1,23 @@
+/**
+ * @file Point.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2014/01/13
+ */
+
+package com.samovich.java.basics.concepts.objects;
+
+public class Point {
+
+    // Public fields.
+    public int x = 0;
+    public int y = 0;
+
+    /*
+     * Constructor method with two parameters.
+     */
+    public Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+}

755805ca49b5c0cbd1c43e64799d48994c0f3a0a
Samovich <valerysamovich@gmail.com>
2015-01-20 03:10:06 -0500

    Sanity

--- src/com/samovich/java/HelloWorldApp.java
+++ src/com/samovich/java/HelloWorldApp.java
@@ -2,7 +2,7 @@
  * @file Template.java
  * @author Valery Samovich
  * @version 1
- * @date 2014/01/09
+ * @date 2015/01/09
  */
 
 package com.samovich.java;
---
+++ src/com/samovich/java/basics/collections/list/ListDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @file ListDemo.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2015/01/20
+ */
+
+package com.samovich.java.basics.collections.list;
+
+public class ListDemo {
+    
+}

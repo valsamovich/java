b36d4ebe83790853352af2aceeefb767fd2a166c
Samovich <valerysamovich@gmail.com>
2015-01-31 03:09:02 -0500

    Implement the Comparable interface.

--- src/main/java/com/samovich/basics/fundamentals/inerfaces/Circle.java
+++ src/main/java/com/samovich/basics/fundamentals/inerfaces/Circle.java
@@ -7,7 +7,7 @@
 
 package com.samovich.basics.fundamentals.inerfaces;
 
-public class Circle {
+public class Circle implements Comparable<Circle>{
     
     // Field.
     private double radius;
@@ -30,6 +30,22 @@ public class Circle {
     @Override
     public String toString() {
         return "C:" + radius;
+    }
+
+    @Override
+    public int compareTo(Circle other) {
+        if(other == null) {
+            throw new NullPointerException("Can't compare with null");
+        }
+        
+        // return (int) Math.signum(radius - other.radius);
+        if(radius > other.radius) {
+            return 1;
+        } else if(radius < other.radius) {
+            return -1;
+        }
+
+        return 0;
     }
     
 }

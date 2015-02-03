f303d071b507fe6b6e41e9ccfec320fd55df8163
Valery <valerysamovich@gmail.com>
2015-02-03 14:26:22 -0500

    Added JUnit test for Circle

---
+++ src/test/java/com/samovich/specialized/junit/circle/CircleTest.java
@@ -0,0 +1,30 @@
+/**
+ * @file CircleTest.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2015/02/03
+ */
+
+package com.samovich.specialized.junit.circle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class CircleTest {
+
+    @Test
+    public void testCircle()  {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testArea() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+}

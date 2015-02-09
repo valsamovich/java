8cbff775d0b25e1c2d9bb088c714c3e823dfdf47
Valery <valerysamovich@gmail.com>
2015-02-09 12:15:57 -0500

    Fix class name

---
+++ src/main/java/com/samovich/basics/fundamentals/strings/GoodbyeDog.java
@@ -0,0 +1,17 @@
+package com.samovich.basics.fundamentals.strings;
+
+public class GoodbyeDog {
+
+    public static void main(String[] args) {
+        
+        String str = "Goodbye, Dog";
+        
+        // str.replace("buy", "");
+        // System.out.println(str);
+        
+        String strNew = str.replace("buy", "");
+        System.out.println(strNew);
+
+    }
+
+}

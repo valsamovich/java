1a9555d0e37e6f7fe51aff9e0f7d3643947e67c6
Samovich <valerysamovich@gmail.com>
2015-02-16 23:29:49 -0500

    Start design pattern package

---
+++ src/main/java/com/samovich/specialized/patterns/singelton/MySingleton.java
@@ -0,0 +1,11 @@
+/**
+ * @file MySingleton.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2015/02/16
+ */
+
+package com.samovich.specialized.patterns.singelton;
+
+public class MySingleton {
+}

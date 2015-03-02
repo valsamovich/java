e260311b2b3d046fe6ea1b1d256cac453bd155df
Valery <valerysamovich@gmail.com>
2015-03-02 17:30:25 -0500

    Added JavaDoc and Add BoxApp class

---
+++ src/main/java/com/samovich/basics/concepts/generics/box/BoxApp.java
@@ -0,0 +1,18 @@
+/**
+ * @file BoxApp.java
+ * @author Valery Samovich
+ * @version 2
+ * @date 08/7/2014
+ */
+
+package com.samovich.basics.concepts.generics.box;
+
+public class BoxApp {
+
+    public static void main(String[] args) {
+
+        // Invoking and Instantiating a Generic Type
+        GenericBox<Integer> integerBox = new GenericBox<>();
+    }
+
+}
--- src/main/java/com/samovich/basics/concepts/generics/box/SimpleBox.java
+++ src/main/java/com/samovich/basics/concepts/generics/box/SimpleBox.java
@@ -1,3 +1,10 @@
+/**
+ * @file SimpleBox.java
+ * @author Valery Samovich
+ * @version 2
+ * @date 08/7/2014
+ */
+
 package com.samovich.basics.concepts.generics.box;
 
 /*

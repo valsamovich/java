5a36753238faa86691dcec994d55dc08928ac336
Valery <valerysamovich@gmail.com>
2015-03-04 17:24:48 -0500

    Finish the Ralatable interface example

--- src/main/java/com/samovich/basics/concepts/inerfaces/relatable/RectanglePlus.java
+++ src/main/java/com/samovich/basics/concepts/inerfaces/relatable/RectanglePlus.java
@@ -28,10 +28,38 @@ public class RectanglePlus implements Relatable {
     }
 
     public RectanglePlus(int w, int h) {
-
+        origin = new Point(0, 0);
+        width = w;
+        height = h;
+    }
+    public RectanglePlus(Point p, int w, int h) {
+        origin = p;
+        width = w;
+        height = h;
     }
 
+    // a method for moving the rectangle
+    public void move(int x, int y) {
+        origin.x = x;
+        origin.y = y;
+    }
+
+    // a method for computing
+    // the area of the rectangle
+    public int getArea() {
+        return width * height;
+    }
+
+    // a method required to implement
+    // the Relatable interface
     public int isLargerThan(Relatable other) {
-        return 0;
+        RectanglePlus otherRect
+                = (RectanglePlus)other;
+        if (this.getArea() < otherRect.getArea())
+            return -1;
+        else if (this.getArea() > otherRect.getArea())
+            return 1;
+        else
+            return 0;
     }
 }

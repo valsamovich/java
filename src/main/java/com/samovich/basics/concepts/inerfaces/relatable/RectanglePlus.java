1398f69eb4b27e597726f0646e83524ae4ff94a0
Samovich <valerysamovich@gmail.com>
2015-03-05 01:23:27 -0500

    Apple formatting

--- src/main/java/com/samovich/basics/concepts/inerfaces/relatable/RectanglePlus.java
+++ src/main/java/com/samovich/basics/concepts/inerfaces/relatable/RectanglePlus.java
@@ -43,23 +43,25 @@ public class RectanglePlus implements Relatable {
         origin.x = x;
         origin.y = y;
     }
-
-    // a method for computing
-    // the area of the rectangle
+    
+    /*
+     * A method for computing the area of the rectangle
+     */
     public int getArea() {
         return width * height;
     }
 
-    // a method required to implement
-    // the Relatable interface
+    /*
+     * A method required to implement the Relatable interface
+     */
     public int isLargerThan(Relatable other) {
-        RectanglePlus otherRect
-                = (RectanglePlus)other;
-        if (this.getArea() < otherRect.getArea())
+        RectanglePlus otherRect = (RectanglePlus)other;
+        if (this.getArea() < otherRect.getArea()) {
             return -1;
-        else if (this.getArea() > otherRect.getArea())
+        } else if (this.getArea() > otherRect.getArea()) {
             return 1;
-        else
-            return 0;
+        } else {
+            return 0; 
+        }
     }
 }

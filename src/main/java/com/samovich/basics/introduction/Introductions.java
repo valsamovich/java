7653596539077f78b41997c041fcf472ff04285b
Samovich <valerysamovich@gmail.com>
2015-04-07 23:51:41 -0400

    Add Introduction.java example from Team Treehouse Tutorial.

---
+++ src/main/java/com/samovich/basics/introduction/Introductions.java
@@ -0,0 +1,14 @@
+/**
+ * @file Introductions.java
+ * @author Valery Samovich
+ * @version 1
+ * @date 2015/04/07
+ */
+
+package com.samovich.basics.introduction;
+
+/**
+ * Created by Samovich on 4/7/2015.
+ */
+public class Introductions {
+}

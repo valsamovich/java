228a09745daf44590ac3eb24fefa10b0ab917fe1
Samovich <valerysamovich@gmail.com>
2015-05-05 23:03:47 -0400

    Add Introduction class for Team Treehouse example

--- src/main/java/com/samovich/basics/introduction/treehouse/Introductions.java
+++ src/main/java/com/samovich/basics/introduction/treehouse/Introductions.java
@@ -5,20 +5,17 @@
  * @date 2015/04/07
  */
 
-/**
- * TeamTreehouse. Java Basics.
- */
 package com.samovich.basics.introduction.treehouse;
-
 
 import java.io.Console;
 
 public class Introductions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NullPointerException{
         Console console = System.console();
         // Welcome to the Introductions program!
-        String firstName = "Valery";
+        String firstName;
+        firstName = console.readLine("What is your name? ");
         // thisIsAnExampleOfCamelCasing
         console.printf("Hello, my name is %s\n", firstName);
         console.printf("%s is learning how to write Java\n", firstName);

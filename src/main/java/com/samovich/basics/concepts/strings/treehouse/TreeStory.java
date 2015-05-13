bf3959569cede79307c89d148bf5a41d7a5c4ad6
Samovich <valerysamovich@gmail.com>
2015-05-12 23:50:48 -0400

    Update Multiple Strings Example for Console and Scanner prototype

--- src/main/java/com/samovich/basics/concepts/strings/treehouse/TreeStory.java
+++ src/main/java/com/samovich/basics/concepts/strings/treehouse/TreeStory.java
@@ -14,24 +14,32 @@ public class TreeStory {
 
     public static void main(String[] args) {
 
-        // Console console = System.console();
-        // __Name is a __adjective__ __noun__.
+        // __Name__ is a __adjective__ __noun__.
         // They are always __adverb__ __verb__.
-        String name = console.readLine("Enter a name:  ");
-        String adjective = console.readLine("Enter an adjective:  ");
-        String noun = console.readLine("Enter a noun:  ");
-        String adverb = console.readLine("Enter an adverb:  ");
-        String verb = console.readLine("Enter a verb ending with -ing:  ");
-        console.printf("Your TreeStory:\n----------------\n");
-        console.printf("%s is a $s %s. ", name, adjective, noun);
-        console.printf("They are always %s %s.\n", adverb, verb);
+
+//        Console console = System.console();
+//        String name = console.readLine("Enter a name:  ");
+//        String adjective = console.readLine("Enter an adjective:  ");
+//        String noun = console.readLine("Enter a noun:  ");
+//        String adverb = console.readLine("Enter an adverb:  ");
+//        String verb = console.readLine("Enter a verb ending with -ing:  ");
+//        console.printf("Your TreeStory:\n----------------\n");
+//        console.printf("%s is a $s %s. ", name, adjective, noun);
+//        console.printf("They are always %s %s.\n", adverb, verb);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter a name:  ");
         String name = scanner.nextLine();
-        System.out.println("Enter an adjective: ");
+        System.out.println("Enter an adjective:  ");
         String adjective = scanner.nextLine();
-        System.out.println(name + " is very " + adjective + "!");
-
+        System.out.println("Enter a noun: ");
+        String noun = scanner.nextLine();
+        System.out.println("Enter an adverb: ");
+        String adverb = scanner.nextLine();
+        System.out.println("Enter a verb ending with -ing:  ");
+        String verb = scanner.nextLine();
+        // Print the prototype
+        System.out.println(name + " is a " + adjective + " " + noun + ".");
+        System.out.println("They are always " + adverb + " " + verb + ".");
     }
 }

9093d5e012be1a7a4bb2477103e5c42dc24a5eec
Samovich <valerysamovich@gmail.com>
2015-05-12 23:33:59 -0400

    Add Multiple Strings Example for Console and Scanner prototype

--- src/main/java/com/samovich/basics/concepts/strings/treehouse/TreeStory.java
+++ src/main/java/com/samovich/basics/concepts/strings/treehouse/TreeStory.java
@@ -13,10 +13,12 @@ import java.util.Scanner;
 public class TreeStory {
 
     public static void main(String[] args) {
+
         // Console console = System.console();
         // String name = console.readLine("Enter your name: ");
         // String adjective = console.readLine("Enter an adjective: ");
         // console.printf("%s is very %s", name, adjective);
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();

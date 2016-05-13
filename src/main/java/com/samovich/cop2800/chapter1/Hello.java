dc959418d01f22f30e3201e93597e5fd92c48725
Valery Samovich <valerysamovich@gmail.com>
2016-05-13 16:36:32 -0400

    Add HelloErrors example

--- src/main/java/com/samovich/cop2800/chapter1/Hello.java
+++ src/main/java/com/samovich/cop2800/chapter1/Hello.java
@@ -1,8 +1,14 @@
 package com.samovich.cop2800.chapter1;
 
+/**
+ * Filename HelloDialog.java
+ * Created by Valery Samovich
+ * Written on 05/13/2016
+ */
+
 public class Hello {
 
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        System.out.println("Hello, World!");
     }
 }
--- src/main/java/com/samovich/cop2800/chapter1/HelloErrors.java
+++
@@ -1,5 +0,0 @@
-package com.samovich.cop2800.chapter1;
-
-public class HelloErrors {
-
-}

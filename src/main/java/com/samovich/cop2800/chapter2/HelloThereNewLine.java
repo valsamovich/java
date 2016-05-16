6b0367e9d9ab95d01cfec06fda618e4c436e0bee
Valery Samovich <valerysamovich@gmail.com>
2016-05-16 14:13:32 -0400

    Add Char examples

---
+++ src/main/java/com/samovich/cop2800/chapter2/HelloThereNewLine.java
@@ -0,0 +1,15 @@
+package com.samovich.cop2800.chapter2;
+
+/**
+ * Filename HelloThereNewLine.java
+ * Created by Valery Samovich
+ * Written on 5/16/16
+ */
+public class HelloThereNewLine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello\nthere");
+        System.out.println("Hello\tthere");
+        System.out.println("Hello\bthere");
+    }
+}

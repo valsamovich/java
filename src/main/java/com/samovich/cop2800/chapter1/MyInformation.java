e5094113c4cf55945c4086d4c8153bf03c7453f0
vsamov <valerysamovich@gmail.com>
2016-05-16 21:12:03 -0400

    Add ConfirmDialog and MaxValues classes

--- src/main/java/com/samovich/cop2800/chapter1/MyInformation.java
+++ src/main/java/com/samovich/cop2800/chapter1/MyInformation.java
@@ -3,14 +3,17 @@ package com.samovich.cop2800.chapter1;
 import javax.swing.JOptionPane;
 
 /**
- * Filename java-technologies
+ * Filename MyInformation.java
  * Created by Valery Samovich
  * Written on 5/14/16
  */
 public class MyInformation {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "My name is Valery Samovich and my major is Computer Programming & Analysis", "Student Info", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,
+                "My name is Valery Samovich and my major is Computer Programming & Analysis",
+                "Student Info",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
 }

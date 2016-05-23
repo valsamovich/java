6ad6d5aa82ed453325dafc48bc290cf043a2fbd9
vsamov <valerysamovich@gmail.com>
2016-05-22 22:40:55 -0400

    Refactor

--- src/main/java/com/samovich/cop2800/Hello.java
+++ src/main/java/com/samovich/cop2800/Hello.java
@@ -1,9 +1,12 @@
-package com.samovich.cop2800.chapter1;
+package com.samovich.cop2800;
 
 /**
  * Filename HelloDialog.java
  * Created by Valery Samovich
  * Written on 05/13/2016
+ *
+ * To compile Java Class: javac Hello.java
+ * Ro run compiled Class: java Hello
  */
 
 public class Hello {

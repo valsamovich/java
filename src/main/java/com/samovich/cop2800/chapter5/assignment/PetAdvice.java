f87b0272a879e4a787181410ac9d29803c2c9ca5
vsamov <valerysamovich@gmail.com>
2016-06-01 00:07:35 -0400

    Start PetAdvice , chapter 5

---
+++ src/main/java/com/samovich/cop2800/chapter5/assignment/PetAdvice.java
@@ -0,0 +1,33 @@
+package com.samovich.cop2800.chapter5.assignment;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Filename PetAdvice.java
+ * Created by Valery Samovich
+ * Written on 5/31/2016
+ */
+public class PetAdvice {
+
+    public static void main(String[] args) {
+    }
+
+    public static int typeOfDwelling(int number){
+
+        // declare variables
+        String dwellingType;
+        int dwellingNumber;
+
+        // prompt the user to accept value
+        dwellingType = JOptionPane.showInputDialog(
+                null,
+                "Enter 1(apartment), 2(house), 3(dorm):",
+                "Dwelling Type",
+                JOptionPane.QUESTION_MESSAGE);
+
+        // convert string to an number
+        dwellingNumber = Integer.parseInt(dwellingType);
+
+        return dwellingNumber;
+    }
+}

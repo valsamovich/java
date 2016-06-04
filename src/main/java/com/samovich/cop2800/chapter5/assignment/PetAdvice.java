21043ecfeb293b51f5827fe6c9c51e30c9832c01
vsamov <valerysamovich@gmail.com>
2016-06-04 15:12:03 -0400

    Add method to get value for hours per week

--- src/main/java/com/samovich/cop2800/chapter5/assignment/PetAdvice.java
+++ src/main/java/com/samovich/cop2800/chapter5/assignment/PetAdvice.java
@@ -12,10 +12,29 @@ public class PetAdvice {
     public static void main(String[] args) {
 
         // declare variables
+        int validDwellingNumber;
+        int validHoursPerWeek;
+
+        // get valid values
+        validDwellingNumber = getDwellingNumber();
+        validHoursPerWeek = getHoursPerWeek();
+
+        // print the entered value
+        System.out.println(validDwellingNumber);
+        System.out.println(validHoursPerWeek);
+    }
+
+    /**
+     * Method design to process user input and return type of dwelling as a
+     * number if it's valid.
+     * @return dwellingNumber
+     */
+    private static int getDwellingNumber() {
+        // declare variables
         String dwellingType;
         int dwellingNumber;
 
-        // prompt the user to accept value
+        // prompt the user to enter the value for dwelling
         dwellingType = JOptionPane.showInputDialog(
                 null,
                 "Enter 1 (apartment), 2 (house), 3 (dorm):",
@@ -26,18 +45,53 @@ public class PetAdvice {
         dwellingNumber = Integer.parseInt(dwellingType);
 
         // test if entered value is valid
-        if (dwellingNumber < 1 || dwellingNumber > 3) {
+        if (dwellingNumber != 1 && dwellingNumber != 2 && dwellingNumber != 3) {
             // display error message
             JOptionPane.showMessageDialog(
                     null,
-                    "The value for dwelling type must be 1 (apartment), 2 (house), or 3 (dorm)",
+                    "The value for dwelling type must be " +
+                            "1 (apartment), 2 (house), or 3 (dorm)",
                     "Dwelling Type Error",
                     JOptionPane.ERROR_MESSAGE);
             // stop the application
             System.exit(0);
         }
+        return dwellingNumber;
+    }
 
-        // print the entered value
-        System.out.println(dwellingNumber);
+    /**
+     * Method design to process user input for hours per week and return value
+     * as number if it's valid.
+     * @return hoursPerWeekAsNumber
+     */
+    private static int getHoursPerWeek(){
+        // declare variables
+        String hoursPerWeek;
+        int hoursPerWeekAsNumber;
+
+        // prompt the user to enter the value for hours
+        hoursPerWeek = JOptionPane.showInputDialog(
+                null,
+                "Enter the number of hours per week you are home:",
+                "Hours at Home",
+                JOptionPane.QUESTION_MESSAGE);
+
+        // convert string to an number
+        hoursPerWeekAsNumber = Integer.parseInt(hoursPerWeek);
+
+        // test if entered value is valid
+        if (hoursPerWeekAsNumber < 0 || hoursPerWeekAsNumber > 168) {
+            // display error message
+            JOptionPane.showMessageDialog(
+                    null,
+                    "The number of hours per week you are home must be " +
+                            "between 0 and 168 inclusive",
+                    "Hours at Home Error",
+                    JOptionPane.ERROR_MESSAGE);
+            // stop the application
+            System.exit(0);
+        }
+        return hoursPerWeekAsNumber;
+
     }
 }

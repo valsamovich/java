b1fca896a4f0353418e72fb6c7b2f20a6cc78885
vsamov <valerysamovich@gmail.com>
2016-06-07 22:08:42 -0400

    Add loops examples

---
+++ src/main/java/com/samovich/cop2800/chapter6/IncrementDemo.java
@@ -0,0 +1,23 @@
+package com.samovich.cop2800.chapter6;
+
+/**
+ * Filename IncrementDemo.java
+ * Created by Valery Samovich
+ * Written on 5/31/16
+ */
+public class IncrementDemo {
+    public static void main(String[] args) {
+        int myNumber, answer;
+        myNumber = 17;
+
+        System.out.println("Before incrementing, myNumber is " + myNumber);
+        answer = ++myNumber;
+        System.out.println("After prefix increment, myNumber is " + myNumber);
+        System.out.println("    and answer is " + answer);
+        myNumber = 17;
+        System.out.println("Before incrementing, myNumber is " + myNumber);
+        answer = myNumber++;
+        System.out.println("After postfix increment, myNumber is " + myNumber);
+        System.out.println("    and answer is " + answer);
+    }
+}

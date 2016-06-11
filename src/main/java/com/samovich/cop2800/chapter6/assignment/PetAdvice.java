fa73fca907b2f5d621b68128dffa97d62e7824a5
vsamov <valerysamovich@gmail.com>
2016-06-11 16:06:45 -0400

    Add support for invalid dwelling and hours

--- src/main/java/com/samovich/cop2800/chapter6/assignment/PetAdvice.java
+++ src/main/java/com/samovich/cop2800/chapter6/assignment/PetAdvice.java
@@ -35,28 +35,29 @@ public class PetAdvice {
         String dwellingType;
         int dwellingNumber;
 
-        // prompt the user to enter the value for dwelling
-        dwellingType = JOptionPane.showInputDialog(
-                null,
-                "Enter 1 (apartment), 2 (house), 3 (dorm).",
-                "Dwelling Type",
-                JOptionPane.QUESTION_MESSAGE);
-
-        // convert string to an number
-        dwellingNumber = Integer.parseInt(dwellingType);
-
-        // test if entered value is valid
-        if (dwellingNumber != 1 && dwellingNumber != 2 && dwellingNumber != 3) {
-            // display error message
-            JOptionPane.showMessageDialog(
+        do {
+            // prompt the user to enter the value for dwelling
+            dwellingType = JOptionPane.showInputDialog(
                     null,
-                    "The value for dwelling type must be " +
-                            "1 (apartment), 2 (house), or 3 (dorm)",
-                    "Dwelling Type Error",
-                    JOptionPane.ERROR_MESSAGE);
-            // stop the application
-            System.exit(0);
-        }
+                    "Enter 1(apartment), 2(house), 3(dorm).",
+                    "Dwelling Type",
+                    JOptionPane.QUESTION_MESSAGE);
+
+            // convert string to an number
+            dwellingNumber = Integer.parseInt(dwellingType);
+
+            // test if entered value is valid
+            if (dwellingNumber != 1 && dwellingNumber != 2 && dwellingNumber != 3) {
+                // display error message
+                JOptionPane.showMessageDialog(
+                        null,
+                        "The value for dwelling type must be " +
+                                "1(apartment), 2(house), or 3(dorm)",
+                        "Dwelling Type Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        } while(dwellingNumber <= 0 || dwellingNumber >= 4);
+
         return dwellingNumber;
     }
 
@@ -69,28 +70,31 @@ public class PetAdvice {
         String hoursPerWeek;
         int hoursPerWeekAsNumber;
 
-        // prompt the user to enter the value for hours
-        hoursPerWeek = JOptionPane.showInputDialog(
-                null,
-                "Enter the number of hours per week you are home.",
-                "Hours at Home",
-                JOptionPane.QUESTION_MESSAGE);
-
-        // convert string to an number
-        hoursPerWeekAsNumber = Integer.parseInt(hoursPerWeek);
-
-        // test if entered value is valid
-        if (hoursPerWeekAsNumber < 0 || hoursPerWeekAsNumber > 168) {
-            // display error message
-            JOptionPane.showMessageDialog(
+        do {
+            // prompt the user to enter the value for hours
+            hoursPerWeek = JOptionPane.showInputDialog(
                     null,
-                    "The number of hours per week you are home must be " +
-                            "between 0 and 168 inclusive",
-                    "Hours at Home Error",
-                    JOptionPane.ERROR_MESSAGE);
-            // stop the application
-            System.exit(0);
-        }
+                    "Enter the number of hours per week you are home.",
+                    "Hours at Home",
+                    JOptionPane.QUESTION_MESSAGE);
+
+            // convert string to an number
+            hoursPerWeekAsNumber = Integer.parseInt(hoursPerWeek);
+
+            // test if entered value is valid
+            if (hoursPerWeekAsNumber < 0 || hoursPerWeekAsNumber > 168) {
+                // display error message
+                JOptionPane.showMessageDialog(
+                        null,
+                        "The number of hours per week you are home must be " +
+                                "between 0 and 168 inclusive",
+                        "Hours at Home Error",
+                        JOptionPane.ERROR_MESSAGE);
+                // stop the application
+                System.exit(0);
+            }
+        } while(hoursPerWeekAsNumber <= 0 || hoursPerWeekAsNumber >= 169);
+
         return hoursPerWeekAsNumber;
     }
 

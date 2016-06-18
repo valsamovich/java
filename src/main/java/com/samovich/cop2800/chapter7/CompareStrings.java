bbc2ee0119031bee83c37030421e1ec745074345
vsamov <valerysamovich@gmail.com>
2016-06-17 21:40:50 -0400

    Add few strings examples

---
+++ src/main/java/com/samovich/cop2800/chapter7/CompareStrings.java
@@ -0,0 +1,26 @@
+package com.samovich.cop2800.chapter7;
+
+import java.util.Scanner;
+
+/**
+ * Filename CompareStrings.java
+ * Created by Valery Samovich
+ * Written on 6/17/2016
+ */
+public class CompareStrings {
+    public static void main(String[] args) {
+        // declare fields
+        String aName = "Carmen";
+        String anotherName;
+        // prompt the user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name > ");
+        anotherName = input.nextLine();
+        // condition
+        if(aName.equals(anotherName)){
+            System.out.println(aName + " equals " + anotherName);
+        } else {
+            System.out.println(aName + " does not equal " + anotherName);
+        }
+    }
+}
---
+++ src/main/java/com/samovich/cop2800/chapter7/RepairName.java
@@ -0,0 +1,43 @@
+package com.samovich.cop2800.chapter7;
+
+import javax.swing.*;
+
+/**
+ * Filename RepairName.java
+ * Created by Valery Samovich
+ * Written on 6/17/2016
+ */
+public class RepairName {
+    public static void main(String[] args) {
+        // declare fields
+        String name, saveOriginalName;
+        int stringLenght;
+        int i;
+        char c;
+        // prompt the user
+        name = JOptionPane.showInputDialog(
+                null,
+                "Please enter your first and last name");
+        saveOriginalName = name;
+        stringLenght = name.length();
+        for(i = 0; i < stringLenght; i++){
+            c = name.charAt(i);
+            if(i == 0){
+                c = Character.toUpperCase(c);
+                name = c + name.substring(1, stringLenght);
+            }else{
+                if(name.charAt(i) == ' '){
+                    ++i;
+                    c = name.charAt(i);
+                    c = Character.toUpperCase(c);
+                    name = name.substring(0, i) + c +
+                            name.substring(i + 1, stringLenght);
+                }
+            }
+        }
+        JOptionPane.showMessageDialog(
+                null,
+                "Original name was " + saveOriginalName +
+                        "\nRepaired name is " + name);
+    }
+}

e16356e1c6ffbbd2b77b78a5f1c2af3d5b906f55
vsamov <valerysamovich@gmail.com>
2016-06-25 23:17:05 -0400

    Start assignment for arrays

---
+++ src/main/java/com/samovich/cop2800/chapter8/Quiz.java
@@ -0,0 +1,34 @@
+package com.samovich.cop2800.chapter8;
+
+/**
+ * Filename Quiz.java
+ * Created by Valery Samovich
+ * Written on 6/25/2016
+ */
+public class Quiz {
+    public static void main(String[] args) {
+        int [] vals = new int[10];
+
+        vals[0] = 1;
+        vals[1] = 2;
+        vals[2] = 3;
+        vals[3] = 4;
+        vals[4] = 5;
+        vals[5] = 6;
+        vals[6] = 7;
+        vals[7] = 8;
+        vals[8] = 9;
+        vals[9] = 10;
+
+        System.out.println(vals[0]);
+        System.out.println(vals[1]);
+        System.out.println(vals[2]);
+        System.out.println(vals[3]);
+        System.out.println(vals[4]);
+        System.out.println(vals[5]);
+        System.out.println(vals[6]);
+        System.out.println(vals[7]);
+        System.out.println(vals[8]);
+        System.out.println(vals[9]);
+    }
+}
---
+++ src/main/java/com/samovich/cop2800/chapter8/assignment/MCChecker.java
@@ -0,0 +1,9 @@
+package com.samovich.cop2800.chapter8.assignment;
+
+/**
+ * Filename MCChecker.java
+ * Created by Valery Samovich
+ * Written on 6/25/2016
+ */
+public class MCChecker {
+}

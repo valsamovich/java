76ccbb68170fa497b7dafd158a5b66feb8662b9d
vsamov <valerysamovich@gmail.com>
2016-07-02 17:27:14 -0400

    Add interface example

---
+++ src/main/java/com/samovich/cop2800/chapter11/InsuredCarDemo.java
@@ -0,0 +1,15 @@
+package com.samovich.cop2800.chapter11;
+
+import javax.swing.*;
+
+/**
+ * Filename InsuredCarDemo.java
+ * Created by Valery Samovich
+ * Written on 7/2/2016
+ */
+public class InsuredCarDemo {
+    public static void main(String[] args) {
+        InsuredCar myCar = new InsuredCar();
+        JOptionPane.showMessageDialog(null, myCar.toString());
+    }
+}

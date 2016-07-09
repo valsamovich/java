642b71e663148d2b8249d875318f7db0c0a8835d
vsamov <valerysamovich@gmail.com>
2016-07-08 23:47:46 -0400

    Add multiple exception example

--- src/main/java/com/samovich/cop2800/chapter12/MultipleException.java
+++ src/main/java/com/samovich/cop2800/chapter12/MultipleException.java
@@ -3,11 +3,11 @@ package com.samovich.cop2800.chapter12;
 import javax.swing.*;
 
 /**
- * Filename ExceptionDemo.java
+ * Filename MultipleException.java
  * Created by Valery Samovich
  * Written on 7/8/2016
  */
-public class ExceptionDemo {
+public class MultipleException {
     public static void main(String[] args) {
         // declare fields
         int numerator = 0;
@@ -18,15 +18,21 @@ public class ExceptionDemo {
         // prompt the user and proceed with calculation
         try {
             inputString = JOptionPane.showInputDialog(null,
-                    "Enter a number to be divided");
+                    "Enter a number to be divided:");
             numerator = Integer.parseInt(inputString);
             inputString  = JOptionPane.showInputDialog(null,
-                    "Enter a number to divide into the first number");
+                    "Enter a number to divide into the first number:");
             denominator = Integer.parseInt(inputString);
             result = numerator / denominator;
         } catch (ArithmeticException exception) {
             JOptionPane.showMessageDialog(null, exception.getMessage());
             result = 0;
+        } catch (NumberFormatException exception) {
+            JOptionPane.showMessageDialog(null,
+                    "This application accepts digits only!");
+            numerator = 999;
+            denominator = 999;
+            result = 1;
         }
 
         // display the result

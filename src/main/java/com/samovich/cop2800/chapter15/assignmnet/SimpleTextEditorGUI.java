aa9d2c12fc4bbe25331f3afadc0638f107828ba6
Valery Samovich <valerysamovich@gmail.com>
2016-07-27 13:19:05 -0400

    Add text filed: NOT COMPLETED

--- src/main/java/com/samovich/cop2800/chapter15/assignmnet/SimpleTextEditorGUI.java
+++ src/main/java/com/samovich/cop2800/chapter15/assignmnet/SimpleTextEditorGUI.java
@@ -2,6 +2,7 @@ package com.samovich.cop2800.chapter15.assignmnet;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  * Contain the code for the interface and event handling
@@ -13,12 +14,31 @@ public class SimpleTextEditorGUI  extends JFrame {
     private JMenuBar mnuBar = new JMenuBar();
     private JMenu mnuFile = new JMenu("Edit");
     private JMenu mnuHelp = new JMenu("Help");
+    JPanel plnContent = new JPanel();
+    JTextArea txtArea = new JTextArea(200, 100);
+    JLabel lblFontSize = new JLabel("Font Size");
+    JTextField txtSetSize = new JTextField();
 
+    /**
+     * Constructor
+     */
     public SimpleTextEditorGUI() {
-        setLayout(new BorderLayout());
         setSize(360, 210);
+        setLayout(new BorderLayout());
         setJMenuBar(mnuBar);
+        txtArea.setLineWrap(true);
+        txtArea.setWrapStyleWord(true);
+        // add constants from keyboard
+        mnuFile.setMnemonic(KeyEvent.VK_F);
+        mnuHelp.setMnemonic(KeyEvent.VK_H);
+        // add menu
         mnuBar.add(mnuFile);
         mnuBar.add(mnuHelp);
+        // text area
+        plnContent.setLayout(new BorderLayout());
+        plnContent.add(txtArea, BorderLayout.EAST);
+        plnContent.add(lblFontSize);
+        plnContent.add(txtSetSize);
+        setContentPane(plnContent);
     }
 }

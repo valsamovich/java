a365aa4e3059b897797c7890f5212cf710da87c2
vsamov <valerysamovich@gmail.com>
2017-02-04 22:27:52 -0500

    Tmp solution for file read

--- src/main/java/org/samovich/cs6310/assignment4/CMSApplication.java
+++ src/main/java/org/samovich/cs6310/assignment4/CMSApplication.java
@@ -1,12 +1,21 @@
 package org.samovich.cs6310.assignment4;
 
+import java.io.*;
+import java.net.URL;
+
 /**
  * Main Class for Course Management System Application.
  * @author  Valery Samovich
  * @see     System
  */
 public class CMSApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello, I'm a CMR!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("./students.txt")));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
+        System.out.println(lines);
     }
 }
